7dea2b53f889ec3039b801d5a8d6a477f75e8ca7
kosmosghost <kosmosghost@proton.me>
2022-12-04 23:37:58 -0800

    Self-signed cert for Alpine

--- create_html.sh
+++ create_html.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-pandoc -s --template TEMPLATE/dark-template.html -o HTML/horrible-linux-guide-dark.html MD/horrible-linux-guide.md
+pandoc -s --template TEMPLATE/dark-template.html -o HTML/horrible-linux-guide-dark.html MD/horrible-linux-guide.md --toc --toc-depth 3
 
-pandoc -s --template TEMPLATE/light-template.html -o HTML/horrible-linux-guide-light.html MD/horrible-linux-guide.md
+pandoc -s --template TEMPLATE/light-template.html -o HTML/horrible-linux-guide-light.html MD/horrible-linux-guide.md --toc --toc-depth 3
